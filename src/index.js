3feaa9c2064fedefadc881efa40f339a91cbd71e
wandji20 <wbertrand10@gmail.com>
2021-05-21 10:03:34 +0100

    Add Boot Scene

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import config from './config/config';
 import StartScene from './scenes/StartScene';
 import GameScene from './scenes/GameScene';
 import GameOverScene from './scenes/GameOverScene';
+import BootScene from './scenes/BootScene';
+import PreloaderScene from './scenes/PreloaderScene';
+import TitleScene from './scenes/TitleScene';
+import OptionsScene from './scenes/OptionsScene';
+import CreditsScene from './scenes/CreditsScene';
 
 class MyShooterGame extends Phaser.Game {
   constructor() {
     super(config);
-
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Options', OptionsScene);
+    this.scene.add('Credits', CreditsScene);
     this.scene.add('StartScene', StartScene);
     this.scene.add('GameScene', GameScene);
     this.scene.add('GameOverScene', GameOverScene);
